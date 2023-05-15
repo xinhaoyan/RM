767b538a58fc70e5c7807315d2e8b57c6c623526
xinhaoyan <111496997+xinhaoyan@users.noreply.github.com>
2023-05-15 15:48:05 +0100

    Add files via upload

---
+++ survis_0.1.0_20151022/src/data/generated/bib.js
@@ -0,0 +1,100 @@
+﻿define({ entries : {
+    "cui2014optimized": {
+        "author": "Cui, Xiaoli and Zhu, Pingfei and Yang, Xin and Li, Keqiu and Ji, Changqing",
+        "journal": "The Journal of Supercomputing",
+        "pages": "1249--1259",
+        "publisher": "Springer",
+        "title": "Optimized big data K-means clustering using MapReduce",
+        "type": "article",
+        "volume": "70",
+        "year": "2014"
+    },
+    "hashem2016mapreduce": {
+        "author": "Hashem, Ibrahim Abaker Targio and Anuar, Nor Badrul and Gani, Abdullah and Yaqoob, Ibrar and Xia, Feng and Khan, Samee Ullah",
+        "journal": "Scientometrics",
+        "pages": "389--422",
+        "publisher": "Springer",
+        "title": "MapReduce: Review and open challenges",
+        "type": "article",
+        "volume": "109",
+        "year": "2016"
+    },
+    "jiang2010performance": {
+        "author": "Jiang, Dawei and Ooi, Beng Chin and Shi, Lei and Wu, Sai",
+        "journal": "Proceedings of the VLDB Endowment",
+        "number": "1-2",
+        "pages": "472--483",
+        "publisher": "VLDB Endowment",
+        "title": "The performance of mapreduce: An in-depth study",
+        "type": "article",
+        "volume": "3",
+        "year": "2010"
+    },
+    "khader2020density": {
+        "author": "Khader, M and Al-Naymat, G",
+        "journal": "ACM Computing Surveys (CSUR)",
+        "title": "Density-based Algorithms for Big Data Clustering Using MapReduce Framework",
+        "type": "article",
+        "year": "2020"
+    },
+    "khezr2017mapreduce": {
+        "author": "Khezr, Seyed Nima and Navimipour, Nima Jafari",
+        "journal": "Journal of Grid Computing",
+        "pages": "295--321",
+        "publisher": "Springer",
+        "title": "MapReduce and its applications, challenges, and architecture: a comprehensive review and directions for future research",
+        "type": "article",
+        "volume": "15",
+        "year": "2017"
+    },
+    "liu2021innovative": {
+        "author": "Liu, Yang and Du, Xinxin and Ma, Shuaifeng",
+        "journal": "Electronic Commerce Research",
+        "pages": "1--31",
+        "publisher": "Springer",
+        "title": "Innovative study on clustering center and distance measurement of K-means algorithm: mapreduce efficient parallel algorithm based on user data of JD mall",
+        "type": "article",
+        "year": "2021"
+    },
+    "magdeldin2012murine": {
+        "author": "Magdeldin, Sameh and Yoshida, Yutaka and Li, Huiping and Maeda, Yoshitaka and Yokoyama, Munesuke and Enany, Shymaa and Zhang, Ying and Xu, Bo and Fujinaka, Hidehiko and Yaoita, Eishin and others",
+        "journal": "BioData mining",
+        "number": "1",
+        "pages": "1--14",
+        "publisher": "BioMed Central",
+        "title": "Murine colon proteome and characterization of the protein pathways",
+        "type": "article",
+        "volume": "5",
+        "year": "2012"
+    },
+    "sardar2018partition": {
+        "author": "Sardar, Tanvir Habib and Ansari, Zahid",
+        "journal": "Future Computing and Informatics Journal",
+        "number": "2",
+        "pages": "247--261",
+        "publisher": "Elsevier",
+        "title": "Partition based clustering of large datasets using MapReduce framework: An analysis of recent themes and directions",
+        "type": "article",
+        "volume": "3",
+        "year": "2018"
+    },
+    "tiwari2015classification": {
+        "author": "Tiwari, Nidhi and Sarkar, Santonu and Bellur, Umesh and Indrawan, Maria",
+        "journal": "ACM Computing Surveys (CSUR)",
+        "number": "3",
+        "pages": "1--38",
+        "publisher": "ACM New York, NY, USA",
+        "title": "Classification framework of MapReduce scheduling algorithms",
+        "type": "article",
+        "volume": "47",
+        "year": "2015"
+    },
+    "zaschke2014ph": {
+        "author": "Z{\\\"a}schke, Tilmann and Zimmerli, Christoph and Norrie, Moira C",
+        "booktitle": "Proceedings of the 2014 ACM SIGMOD international conference on Management of data",
+        "pages": "397--408",
+        "title": "The PH-tree: a space-efficient storage structure and multi-dimensional index",
+        "type": "inproceedings",
+        "year": "2014"
+    }
+}});
